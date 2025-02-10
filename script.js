a13fe5bfb42f466adb8cb50e79cab2143e98d0ed
ioangheraszim <gijohny17@gmail.com>
2025-02-10 13:33:25 +0000

    Add input variables and constructor prototype in script.js - Added variables for input elements - Implemented prototype for constructor

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+const nameInput = document.getElementById("name");
+const authorInput = document.getElementById("author");
+const imageInput = document.getElementById("image");
+
+// Array to hold objects
 const myLibrary = [];
 
+// Constructor
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.info = function() {
-    return `This book is called ${this.title} and it is written by ${author} and it has ${pages}`;
-  }
 
 }
 
-const book1 = new Book('Harry Potter', 'J.K.Rowling', '1-400');
-const book2 = new Book("Dragon ball Z", 'Akira Toriyama', "1kk")
+Book.prototype.info = function() {
+  return `This book is called ${this.title} and it is written by ${author} and it has ${pages}`;
+}
 
 console.log(book1.info());
 console.log(book2.info());
