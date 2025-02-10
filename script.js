ce1d8a7b92d1204cb62210666f2e35ba90b58992
ioangheraszim <gijohny17@gmail.com>
2025-02-10 13:57:47 +0000

    Add functions to manage and display library books - Added function to add books to the library - Added function to display books on the page

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 const nameInput = document.getElementById("name");
 const authorInput = document.getElementById("author");
 const imageInput = document.getElementById("image");
+const addBtn = document.getElementById("add-btn");
 
 // Array to hold objects
 const myLibrary = [];
 
 // Constructor
-function Book(title, author, pages) {
+function Book(title, author, image) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
-
+  this.image = image;
 }
 
 Book.prototype.info = function() {
-  return `This book is called ${this.title} and it is written by ${author} and it has ${pages}`;
+  return `${this.title} ${this.author} ${this.image} `;
 }
-
-console.log(book1.info());
-console.log(book2.info());
 
 function addBookToLibrary() {
   // take params, create a book then store it in the array
+
+  let nameValue = nameInput.value;
+  let authorValue = authorInput.value;
+  let imageValue = imageInput.value;
+
+  if (nameValue === "" || authorValue === "" || imageValue === "") {
+    alert("Please enter all the correct values")
+  }
+
+  let newBook = new Book (nameValue, authorValue, imageValue);
+  myLibrary.push(newBook);
+  displayBooks();
+}
+
+function displayBooks() {
+  const books = document.querySelector(".book-cards");
+  books.innerHTML = "";
+
+  myLibrary.forEach((book, index) => {
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    let myImage = document.createElement("img");
+    myImage.classList.add("book-card__image");
+    myImage.src = book.image;
+
+    let myTitle = document.createElement("p");
+    myTitle.textContent = book.title;
+    myTitle.classList.add("book-card__title");
+
+    let myAuthor = document.createElement("p");
+    myAuthor.classList.add("book-card__author");
+    myAuthor.textContent = book.author;
+
+    bookCard.appendChild(myImage);
+    bookCard.appendChild(myTitle);
+    bookCard.appendChild(myAuthor);
+    books.appendChild(bookCard);
+  });
 }
